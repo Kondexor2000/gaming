7a125db215a1594b51ee2507fb3a871f0ce640db
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2024-11-01 21:37:01 +0100

    Add Docker and Kubernetes

--- gaming/settings.py
+++ gaming/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-622vls##2a9z^7j4(l-+w@xh7e+*5=f!dc*=#jblvoq#v-l!o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
